589c50cfa78f2583623f1e043f882cdcb38f017c
Hernando Cortina <hch@alum.mit.edu>
2024-03-10 18:25:05 -0400

    Modules 1-3

--- test.py
+++ test.py
@@ -3,8 +3,17 @@ print(sys.executable)
 
 import pandas as pd
 
-df = pd.DataFrame({"a": range(5)})
-print(df)
 
-
+df=pd.DataFrame({'test': [1,2,3,4],"names": ['a','b','c','d']})
 df.describe()
+
+type(df)
+df['names']
+
+GDP_df = pd.DataFrame({'Country ID': ['USA', 'CHN' , 'IND', 'ARE', 'CAN', 'MEX'], 
+                       'Country':['United States', 'China', 'India', 'United Arab Emirates', 'Canada', 'Mexico'], 
+                       'GDP Per Capita [$]':[69375, 11891, 2116, 43538, 52791, 9967], 
+                       'Global Rank':[5, 64, 150, 24, 15, 72]})
+
+
+print("hello")
